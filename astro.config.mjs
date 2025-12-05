fda7172778f5696ea20e03e1b1d00fc09f91fb42
mcgrizzz <2106813+mcgrizzz@users.noreply.github.com>
2025-12-05 16:13:26 -0500

    Set site and base URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mcgrizzz.github.io',
+  base: '/yomine-docs',
   integrations: [
       starlight({
           title: '⚠️WIP⚠️',
