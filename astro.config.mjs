3dcc61d5ac653e664ead663215e06b1ad6b7fcc1
mcgrizzz <2106813+mcgrizzz@users.noreply.github.com>
2025-12-05 21:27:28 -0500

    Remove Example Guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,6 @@ export default defineConfig({
               {
                   label: 'Guides',
                   items: [
-                      { label: 'Example Guide', slug: 'guides/example' },
                       { label: 'Initial Setup', slug: 'guides/initial-setup'}
                   ],
               },
